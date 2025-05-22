4b56a9bb6c35522edf8efb7f8b8e45f59d39ffe4
IonutParau <ionut.alex.parau@gmail.com>
2025-05-22 17:03:03 +0200

    nule the Zig version

--- src/engine.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-pub const Computer = @import("engine/computer.zig");
-pub const Component = @import("engine/component.zig");
-pub const Universe = @import("engine/universe.zig");
